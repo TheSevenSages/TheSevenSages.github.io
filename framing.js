425e97815679f2f51501ae0f71e7284bc97652d1
TheSevenSages <Jacobrobinson4321@gmail.com>
2023-03-20 17:46:49 -0400

    Formated portfolio page

---
+++ framing.js
@@ -0,0 +1,12 @@
+class heights {
+        homeHeight;
+        aboutHeight;
+        portfolioHeight;
+        contactHeight;
+}
+
+function updateHeight(file, newHeight) {
+        console.log(newHeight);
+        homeHeight = newHeight;
+        console.log(homeHeight);
+}
