efde09c66d5861b02d7f2576f71ed43a2d0d36e9
TheSevenSages <Jacobrobinson4321@gmail.com>
2023-05-03 13:57:59 -0400

    Moved Scripts
    Moved Modal scripts from portfolio page to format.js, and added home as default page.

--- js/format.js
+++ js/format.js
@@ -2,6 +2,8 @@
 // on document load...
 $(document).ready(function(){
 
+        ChangeContent($('#menu_home'));
+
         $('.menu_link').click(function(){
                 ChangeContent(this);
         });
@@ -13,10 +15,27 @@ function ChangeContent(source) {
         var sourceFile = $(source).attr('id').replace('menu_', '');
         sourceFile = sourceFile.replace('..', '') + '.html';
         $.get('content/' + sourceFile, function(data) {
-                //alert(data);
                 $('#content').html(data); 
         });
-        //alert(sourceFile);
 }
+
+ // Get the modal
+ var modal = document.getElementById("myModal");
+ var modalImg = document.getElementById("img01");
+ var captionText = document.getElementById("caption");
+ function activateModal(el){
+         modalImg.style.paddingTop = (el.parentElement.parentElement.offsetTop) + "px";
+         modal.style.display = "block";
+         modalImg.src = el.src;
+         captionText.innerHTML = el.alt;
+ }
+
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+
+ // When the user clicks on <span> (x), close the modal
+ modal.onclick = function() { 
+         modal.style.display = "none";
+ }
 
 
